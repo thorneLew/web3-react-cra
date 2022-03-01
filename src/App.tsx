5ae1fe14c9120eea96e573b9b98045b3f3269deb
Thone Liu <thorne_lew@126.com>
2022-03-01 11:50:38 +0800

    init web3-react-demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css'
+import MetaMaskCard from './components/connectors/MetaMaskCard'
+import NetworkCard from './components/connectors/NetworkCard'
+import PriorityExample from './components/connectors/PriorityExample'
+import WalletConnectCard from './components/connectors/WalletConnectCard'
+import WalletLinkCard from './components/connectors/WalletLinkCard'
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={(window as any).__POWERED_BY_QIANKUN__ ? '/react2' : '/'}>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">sub-react1-Home</Link>
+            </li>
+            <li>
+              <Link to="/about">sub-react1-About</Link>
+            </li>
+            <li>
+              <Link to="/users">sub-react1-Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <>
+    <h2>react2 - Home</h2>;
+    <PriorityExample />
+    <MetaMaskCard />
+    <WalletConnectCard />
+    <WalletLinkCard />
+    <NetworkCard />
+  </>
+  
+}
+
+function About() {
+  return <h2>react2 - About</h2>;
+}
+
+function Users() {
+  return <h2>react2 - Users</h2>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,43 @@
+import './public-path' 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function render(props: any) {
+  const { container } = props;
+  ReactDOM.render(<App />, container ? container.querySelector('#root') : document.querySelector('#root'));
+}
+
+
+if (!(window as any)?.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log('[react17] react app bootstraped');
+}
+
+
+export async function mount(props: any) {
+  console.log('[react17] props from main framework', props);
+  render(props);
+}
+
+export async function unmount(props: any) {
+  const { container } = props;
+  ReactDOM.unmountComponentAtNode(container ? container.querySelector('#root') : document.querySelector('#root'));
+}
+  
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
